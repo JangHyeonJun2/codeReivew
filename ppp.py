c29f036888e95c45bc97a8bc985d0fdcf68e71b1
SAMSUNG\user <dlaekgml15@naver.com>
2021-03-05 19:59:53 +0900

    수정

--- ppp.py
+++ ppp.py
@@ -3,6 +3,6 @@ def calc():
     y = int(input())
 
     for i in range (len(str(y))):
-        result: int(y/(1*10**i))-int(y/(10*10**i))*10
+        result = int(y/(1*10**i))-int(y/(10*10**i))*10
         print(x*result)
     print(x*y)
