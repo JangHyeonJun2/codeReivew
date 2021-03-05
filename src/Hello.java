5f57723154195a4629ca1602ab03fb37c2138f47
SAMSUNG\user <dlaekgml15@naver.com>
2021-03-05 14:32:14 +0900

    변경했습니다

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("print");
         System.out.println("hello");
-         System.out.println("good");
+        System.out.println("good");
+        System.out.println("1111111");
     }
 }
