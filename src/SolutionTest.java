b86724a7c6c24555c46987e4ce33e6b665388764
JangHyeonJun2 <epfvkdlxj@naver.com>
2021-03-05 15:09:45 +0900

    10869 문제 풀었습니다!

---
+++ src/SolutionTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+    Solution s = new Solution();
+
+    @Test
+    public void Solution() {
+
+
+    }
+}
