afe3724237bb6e3ee07de99a792e97519ba56292
JangHyeonJun2 <epfvkdlxj@naver.com>
2021-03-05 17:36:01 +0900

    조금 더 코드를 간추린 방식

--- src/Q1110.java
+++ src/Q1110.java
@@ -7,12 +7,16 @@ public class Q1110 {
         int count = 0;
 
         do {
-            int modulus = num1 / 10; // num1이 26이면 2
-            int quotient = num1 % 10; // num1이 26이면 6
-            tmpValue = modulus + quotient; // 8
-            num1 = (quotient * 10) + (tmpValue % 10); //60 + 8
+//            int modulus = num1 / 10; // num1이 26이면 2
+//            int quotient = num1 % 10; // num1이 26이면 6
+//            tmpValue = modulus + quotient; // 8
+//            num1 = (quotient * 10) + (tmpValue % 10); //60 + 8
+
+
+            num1 = ((num1 % 10) * 10) + ((( num1 / 10 ) + ( num1 % 10 )) % 10);
             count++;
         }while(tmp != num1);
+
 
         System.out.println(count);
     }
